df959098834cb3aed470cc1088477e2eb766559e
pawieee <p.agujitas.534827@umindanao.edu.ph>
2025-02-24 08:56:15 +0800

    dynamic options

--- sting.asm
+++ sting.asm
@@ -11,9 +11,12 @@ includelib \masm32\lib\masm32.lib
 
 .data
     file_handle HANDLE ?
-    filename db "D:\acads\3rd Year\2nd SEM\1st Term\CS 11\masm files\assembly_blis", 0
     file_contents db 100000 dup(?)
     bytes_read DWORD ?
+
+    _menu1 db "[1] View Prospectus", 10,0
+    _menu2 db "[2] Enroll Courses", 10,0
+    _menu3 db "[3] Exit", 10,0
 
     _msg1 db "Choose Year: ", 0
     _first_year db "[1] 1st Year", 10,0
@@ -32,39 +35,62 @@ includelib \masm32\lib\masm32.lib
 
     sem1 db "\sem1", 0
     sem2 db "\sem2", 0
+
+    prospec db "\sem\prospectus.txt", 0
     
     file_loc db 1000 dup(?)
     
 .data?
+    menu db 3 dup(?)
     year db 3 dup(?)
     sem db 3 dup(?)
 
-
-
 .code
 start:
-    invoke StdOut, addr _first_year
-    invoke StdOut, addr _second_year
-    invoke StdOut, addr _third_year
-    invoke StdOut, addr _fourth_year
-    invoke StdOut, addr _msg1
-    invoke StdIn, addr year, 3
+    invoke ClearScreen
 
-    invoke StdOut, addr _first_sem
-    invoke StdOut, addr _second_sem
-    invoke StdOut, addr _msg2
-    invoke StdIn, addr sem, 3
+    invoke StdOut, addr _menu1
+    invoke StdOut, addr _menu2
+    invoke StdOut, addr _menu3
+    invoke StdIn, addr menu, 3
 
-    .if year == "1"
-        .if sem == "1"
-            push offset filename
-            push offset sem1
-            push offset yr1
-            push offset file_loc
-            call create_dest
+   ; get current directory
+    invoke GetCurrentDirectory, 1000, addr file_loc
+
+    .if menu == '1'
+        invoke lstrcat, addr file_loc, addr prospec
+    .elseif menu == '2'
+        invoke StdOut, addr _first_year
+        invoke StdOut, addr _second_year
+        invoke StdOut, addr _third_year
+        invoke StdOut, addr _fourth_year
+        invoke StdOut, addr _msg1
+        invoke StdIn, addr year, 3
+
+        invoke StdOut, addr _first_sem
+        invoke StdOut, addr _second_sem
+        invoke StdOut, addr _msg2
+        invoke StdIn, addr sem, 3
+
+        .if sem == '1'
+            invoke lstrcat, addr file_loc, addr sem1
+        .elseif sem == '2'
+            invoke lstrcat, addr file_loc, addr sem2
         .endif
+        .if year == '1'
+            invoke lstrcat, addr file_loc, addr yr1
+        .elseif year == '2'
+            invoke lstrcat, addr file_loc, addr yr2
+        .elseif year == '3'
+            invoke lstrcat, addr file_loc, addr yr3
+        .elseif year == '4'
+            invoke lstrcat, addr file_loc, addr yr4
+        .endif
+    .elseif menu == '3'
+        invoke ExitProcess, 0
     .endif
 
+    invoke StdOut, addr file_loc
     create_file:
         push 0
         push FILE_ATTRIBUTE_NORMAL
@@ -86,22 +112,5 @@ start:
 
     invoke ClearScreen
     invoke StdOut, offset file_contents
-create_dest proc param1:DWORD, param2:DWORD, param3:DWORD, param4:DWORD
-    push param1
-    push param4
-    call szCatStr
-
-    push param3
-    push param4
-    call szCatStr
-
-    push param2
-    push param4
-    call szCatStr
-
-
-    push param4
-    call StdOut
-create_dest endp
     invoke ExitProcess, 0
 end start
